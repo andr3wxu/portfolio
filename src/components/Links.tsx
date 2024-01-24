ed8b887fc3a975f1f986d545dec57ecd6ef4a23c
Andrew Xu <andr3w.xu@gmail.com>
2024-01-24 00:50:30 -0500

    added tech stack, moved links to footer

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -19,7 +19,7 @@ const Links = () => {
     },
   ];
   return (
-    <ul className="p-3 pt-10 flex flex-row">
+    <ul className="p-1 flex flex-row justify-center absolute bottom-0 left-1/2 transform -translate-x-1/2 w-screen bg-blue-600">
       {links.map(({ name, link, icon: Icon }) => {
         return (
           <li key={name}>
@@ -27,7 +27,7 @@ const Links = () => {
               href={link}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex flex-row items-center justify-center mx-3 font-light text-blue-600 text-sm px-1 hover:text-white hover:bg-blue-600 "
+              className="flex flex-row items-center justify-center mx-3 font-light text-blue-600 text-white text-sm px-1 hover:text-blue-600 hover:bg-white "
             >
               <Icon className="mr-1" />
               {name}
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -12,7 +12,7 @@ const Page = () => {
     setLoaded(isLoaded);
   };
   return (
-    <>
+    <div className="w-full">
       <div
         style={{
           transitionDuration: "0.7s",
@@ -22,8 +22,7 @@ const Page = () => {
       >
         <Logo />
       </div>
-      <div className="z-10 relative">
-        <div className="absolute">{loaded}</div>
+      <div className="z-10 mt-60">
         <div
           style={{
             transitionDuration: "0.7s",
@@ -31,22 +30,47 @@ const Page = () => {
             transitionProperty: "opacity",
           }}
         >
-          <Title />
-          <section id="subtitle">
-            i'm a software engineering student @
-            <a
-              href="https://uwaterloo.ca/"
-              target="_blank"
-              className="text-blue-600 px-1 font-semibold hover:bg-blue-600 hover:text-white"
+          <div
+            id="body"
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          >
+            <Title />
+            <section id="subtitle">
+              i'm a software engineering student @
+              <a
+                href="https://uwaterloo.ca/"
+                target="_blank"
+                className="text-blue-600 p font-semibold hover:bg-blue-600 hover:text-white"
+              >
+                uwaterloo
+              </a>
+            </section>
+            <section
+              id="about"
+              className="text-xs text-gray-400 max-w-lg mt-6 leading-5"
             >
-              uwaterloo
-            </a>
-          </section>
+              <p>
+                I mainly work with{" "}
+                <span className="text-blue-500 font-semibold">TypeScript</span>,{" "}
+                <span className="text-sky-600 font-semibold">Node.js</span>, and{" "}
+                <span className="text-blue-400 font-semibold">React</span>, but
+                I've also used{" "}
+                <span className="font-semibold text-gray-600">SQL</span>,{" "}
+                <span className="font-semibold text-gray-600">Django</span>,{" "}
+                <span className="font-semibold text-gray-600">OpenCV</span>, and{" "}
+                <span className="font-semibold text-gray-600">NumPy</span> in
+                the past. Feel free to connect with me via{" "}
+                <span className="font-semibold">email</span> or{" "}
+                <span className="font-semibold">LinkedIn</span>!
+              </p>
+              <p className="mt-3"></p>
+            </section>
+          </div>
           <Links />
         </div>
       </div>
       <TitleCard loadedState={handleLoaded} />
-    </>
+    </div>
   );
 };
 

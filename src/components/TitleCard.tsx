e2d26873d14d683b32e68b2b443c53facce04042
Andrew Xu <andr3w.xu@gmail.com>
2024-08-17 23:43:32 -0600

    add titlecard animation

--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -1,17 +1,63 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
-type LoadedStateFunction = (loaded: boolean) => void;
+type LoadedStateFunction = () => void;
 interface Props {
   loadedState: LoadedStateFunction;
 }
 
 const TitleCard = ({ loadedState }: Props) => {
+  const nameArray = ["a", "n", "d", "r", "3", "w"];
+  const [loaded, setLoaded] = useState(false);
+  const [iterator, setIterator] = useState(0);
+  const [tempArray, setTempArray] = useState(["*", " ", "*", " ", "*", " "]);
+
+  const genChar = () => {
+    const charset = "abcdefghijklmnopqrstuvwxyz1234567890";
+    const random = Math.floor(Math.random() * charset.length);
+    return charset.charAt(random);
+  };
+
+  const updateTemp = () => {
+    const newTempArray = tempArray.map((letter, index) =>
+      nameArray[index] !== letter || iterator < 20 ? genChar() : letter,
+    );
+    setIterator((prevIterator) => prevIterator + 1);
+    setTempArray(iterator < 50 ? newTempArray : nameArray);
+  };
+
   useEffect(() => {
-    console.log(1);
-  });
+    if (!loaded) {
+      const interval = setInterval(() => {
+        updateTemp();
+        if (
+          JSON.stringify(tempArray) === JSON.stringify(nameArray) ||
+          iterator >= 50
+        ) {
+          clearInterval(interval);
+          setTimeout(() => {
+            setLoaded(true);
+            loadedState();
+          }, 1000);
+        }
+      }, iterator * 2);
+      return () => clearInterval(interval);
+    }
+  }, [tempArray, nameArray, iterator, loaded]);
+
   return (
     <>
-      <div className={"font-bold"}>{loadedState.name}</div>
+      <div>
+        {tempArray.map((letter, index) => {
+          return index == 3 ? (
+            <span key={index}>
+              <br />
+              <span>{letter}</span>
+            </span>
+          ) : (
+            <span key={index}>{letter}</span>
+          );
+        })}
+      </div>
     </>
   );
 };

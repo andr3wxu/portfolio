bc8c61b6045cb7a54e423489ef7542a0cf9063e4
Andrew Xu <andr3w.xu@gmail.com>
2025-04-12 19:53:10 -0600

    change current job

--- src/components/Subtitle.tsx
+++ src/components/Subtitle.tsx
@@ -18,13 +18,13 @@ const Subtitle = ({ loadedState }: Props) => {
       className={`${loaded ? "animate-SubtitleMotion" : "opacity-0"} font-normal text-sm p-5 mt-custom-p-top`}
     >
       <p className={`p-5 leading-5`}>
-        Currently <span className={`font-semibold`}>SDE</span> at{" "}
+        Currently <span className={`font-semibold`}>SWE</span> at{" "}
         <a
-          href={"https://www.viridiengroup.com"}
+          href={"https://www.sectorgrowth.ca"}
           target="_blank"
           className={`font-slab text-custom-a font-semibold hover:bg-theme-blue hover:text-theme-off-white dark:hover:bg-theme-off-white dark:hover:text-theme-blue transition duration-200`}
         >
-          <span className={`font-mono font-light`}>[</span>Viridien
+          <span className={`font-mono font-light`}>[</span>Sector Growth
           <span className={`font-mono font-light`}>]</span>
         </a>
         . Check out my projects on{" "}
